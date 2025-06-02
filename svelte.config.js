517cde29e2a99a30467e122354411b6bd201ce1d
Jan Johannes <jan@ntr.io>
2025-06-02 22:37:12 +0300

    drag regions and style fixxes

--- svelte.config.js
+++ svelte.config.js
@@ -4,4 +4,9 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  // compilerOptions: { npm i https://pkg.pr.new/svelte@async
+  //   experimental: {
+  //     async: true
+  //   }
+  // },
 }
